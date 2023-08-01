c200182706f325d78ca9c44e1f6bbab8ff09bec7
nianyi <2245310198@qq.com>
2023-08-01 11:52:53 +0800

    test: 测试 Hello.java

---
+++ java-base/src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * 环境测试
+ *
+ * @author nianyi
+ * @date 2023/08/01 11:48
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!!");
+    }
+}
